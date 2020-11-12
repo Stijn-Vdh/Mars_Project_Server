0108d17e39eccdeaf5ea052441059737191cadcb
maarten.vercruysse <maarten.vercruysse@student.howest.be>
2020-11-12 12:22:16 +0100

    Add controller functionality for createUser

--- src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
+++ src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
@@ -1,7 +1,21 @@
 package be.howest.ti.mars.logic.controller;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class MarsController {
+    Set<BaseAccount> accounts = new HashSet<>();
+
     public String getMessage() {
         return "Hello, Mars!";
+    }
+
+    public byte[] createUser(String name, String password, String endpoint, String address){
+        BaseAccount account = new UserAccount(name, password, endpoint, address);
+
+        if(!accounts.add(account)){ // username exists already
+            //throw error
+        }
+        return account.getUserToken().getToken();
     }
 }
--- src/main/java/be/howest/ti/mars/logic/controller/UserAccount.java
+++ src/main/java/be/howest/ti/mars/logic/controller/UserAccount.java
@@ -5,11 +5,11 @@ import be.howest.ti.mars.logic.controller.security.UserToken;
 public class UserAccount extends BaseAccount {
 
 
-    public UserAccount(String homeAddressEndpoint, String password, String username, String address) {
+    public UserAccount( String username, String password, String homeAddressEndpoint, String address) {
         super(homeAddressEndpoint, password, username, address);
     }
 
-    public UserAccount(UserToken userToken, String homeAddressEndpoint, String password, String username, String address) {
+    public UserAccount(String username, String password, String homeAddressEndpoint, String address, UserToken userToken) {
         super(userToken, homeAddressEndpoint, password, username, address);
     }
 }
