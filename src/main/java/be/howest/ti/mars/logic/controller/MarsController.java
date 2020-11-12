f8d5c46765d639fbb89dbf698a8ca8df7898b46e
maarten.vercruysse <maarten.vercruysse@student.howest.be>
2020-11-12 18:00:30 +0100

    Add AuthenticationException and logic to controller + WebServer

--- src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
+++ src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
@@ -1,6 +1,7 @@
 package be.howest.ti.mars.logic.controller;
 
-import be.howest.ti.mars.logic.controller.exceptions.UsernameIsTakenException;
+import be.howest.ti.mars.logic.controller.exceptions.AuthenticationException;
+import be.howest.ti.mars.logic.controller.exceptions.UsernameException;
 import be.howest.ti.mars.logic.controller.security.UserToken;
 
 import java.util.HashSet;
@@ -17,22 +18,21 @@ public class MarsController {
         BaseAccount account = new UserAccount(name, password, endpoint, address);
 
         if (!accounts.add(account)) { // username exists already
-            throw new UsernameIsTakenException(name + " is already taken");
+            throw new UsernameException("Username (" + name + ") is already taken");
         }
         return account.getUserToken().getToken();
     }
 
-    public byte[] login(String name, String password){
+    public byte[] login(String name, String password) {
         BaseAccount account = accounts.stream()
                 .filter(acc -> acc.getUsername().equals(name) && acc.getPassword().equals(password))
                 .findAny().orElse(null);
 
-       if (account == null){   //throw error
-           throw new RuntimeException();
-
-       }else{
-           account.setUserToken(new UserToken()); // sets a new token, invalidates previous set tokens
-           return account.getUserToken().getToken();
-       }
+        if (account == null) {   // pw and name doesnt match
+            throw new AuthenticationException("Credentials does not match!");
+        } else {
+            account.setUserToken(new UserToken()); // sets a new token, invalidates previous set tokens
+            return account.getUserToken().getToken();
+        }
     }
 }
