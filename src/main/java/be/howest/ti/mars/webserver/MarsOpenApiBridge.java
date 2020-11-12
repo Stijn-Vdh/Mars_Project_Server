f13df8facf7e492278a36a30b821007e04ac8187
maarten.vercruysse <maarten.vercruysse@student.howest.be>
2020-11-12 15:29:35 +0100

    Add createUser to the bridge

--- src/main/java/be/howest/ti/mars/webserver/MarsOpenApiBridge.java
+++ src/main/java/be/howest/ti/mars/webserver/MarsOpenApiBridge.java
@@ -1,10 +1,13 @@
 package be.howest.ti.mars.webserver;
 
 import be.howest.ti.mars.logic.controller.MarsController;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
+import java.util.logging.Logger;
 
 class MarsOpenApiBridge {
     private final MarsController controller;
+    private static final Logger logger = Logger.getLogger(MarsOpenApiBridge.class.getName());
 
     MarsOpenApiBridge() {
         this.controller = new MarsController();
@@ -12,5 +15,15 @@ class MarsOpenApiBridge {
 
     public Object getMessage(RoutingContext ctx) {
         return controller.getMessage();
+    }
+
+    public Object createUser(RoutingContext ctx) {
+        logger.info("createUser");
+
+        JsonObject json = ctx.getBodyAsJson();
+        return controller.createUser(json.getString("name"),
+                json.getString("password"),
+                json.getString("homeAddress"),
+                String.valueOf(json.getInteger("homeEndpointID")));
     }
 }
