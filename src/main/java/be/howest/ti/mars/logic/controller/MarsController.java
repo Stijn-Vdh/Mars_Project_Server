a96aa22b2b33f26ae3bf562410a09f44208b77d1
beheerder <stijn.vanderherten@telenet.be>
2020-11-12 11:48:16 +0100

    Get all subscriptions method in the controller

--- src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
+++ src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
@@ -1,7 +1,12 @@
 package be.howest.ti.mars.logic.controller;
 
+import java.util.Set;
+
 public class MarsController {
     public String getMessage() {
         return "Hello, Mars!";
+    }
+    public Set<Subscription> getAllSubscriptions(){
+        return main.getAllSubscriptions;
     }
 }
