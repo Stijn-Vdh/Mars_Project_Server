448cbed79e90368cd86e826f58e6b785c3877d60
stijn.van.der.herten <stijn.van.der.herten@student.howest.be>
2020-11-29 00:04:20 +0100

    Implemented endpoints repo code

--- src/main/java/be/howest/ti/mars/logic/data/repositories/EndpointsRepository.java
+++ src/main/java/be/howest/ti/mars/logic/data/repositories/EndpointsRepository.java
@@ -2,23 +2,78 @@ package be.howest.ti.mars.logic.data.repositories;
 
 import be.howest.ti.mars.logic.controller.Endpoint;
 import be.howest.ti.mars.logic.controller.converters.ShortEndpoint;
+import be.howest.ti.mars.logic.controller.exceptions.DatabaseException;
+import be.howest.ti.mars.logic.controller.exceptions.EndpointException;
+import be.howest.ti.mars.logic.data.MarsConnection;
+import be.howest.ti.mars.logic.data.MarsH2Repository;
 import be.howest.ti.mars.logic.data.repoInterfaces.EndpointsRepoInt;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class EndpointsRepository implements EndpointsRepoInt {
+    private static final Logger LOGGER = Logger.getLogger(MarsH2Repository.class.getName());
+
+    // Endpoints
+    private static final String SQL_GET_ENDPOINT = "SELECT * FROM ENDPOINTS WHERE ID = ?";
+    private static final String SQL_GET_ENDPOINTS = "SELECT * FROM ENDPOINTS";
+    private static final String SQL_INSERT_ENDPOINT = "INSERT INTO ENDPOINTS(name) VALUES(?)";
+
+    public static final String DESTINATION = "destination";
+    public static final String DATE_TIME = "dateTime";
+
     @Override
     public Set<ShortEndpoint> getEndpoints() {
-        return null;
+        Set<ShortEndpoint> endpoints = new HashSet<>();
+
+        try (Connection con = MarsConnection.getConnection();
+             PreparedStatement stmt = con.prepareStatement(SQL_GET_ENDPOINTS)) {
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    endpoints.add(new ShortEndpoint(rs.getInt("id"), rs.getString("name")));
+                }
+            }
+        } catch (SQLException ex) {
+            LOGGER.log(Level.WARNING, ex.getMessage(), ex);
+            throw new DatabaseException("Cannot retrieve endpoints");
+        }
+        return endpoints;
     }
 
     @Override
     public void addEndpoint(String endpoint) {
-
+        try (Connection con = MarsConnection.getConnection();
+             PreparedStatement stmt = con.prepareStatement(SQL_INSERT_ENDPOINT)) {
+            stmt.setString(1, endpoint);
+            stmt.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DatabaseException("Can't add endpoint!");
+        }
     }
 
     @Override
     public Endpoint getEndpoint(int id) {
-        return null;
+        try (Connection con = MarsConnection.getConnection();
+             PreparedStatement stmt = con.prepareStatement(SQL_GET_ENDPOINT)
+        ) {
+            stmt.setInt(1, id);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return new Endpoint(id, rs.getString("name"), true, "todo", false);
+                } else {
+                    throw new EndpointException("Endpoint with ID (" + id + ") doesn't exist!");
+                }
+            }
+        } catch (SQLException ex) {
+            LOGGER.log(Level.WARNING, ex.getMessage(), ex);
+            throw new DatabaseException("Cannot retrieve endpoint with id: " + id + "!");
+        }
     }
 }
