3b1fce649d61c5d1260bfef1ba44d510136b8dfa
maarten.vercruysse <maarten.vercruysse@student.howest.be>
2020-12-03 22:12:50 +0100

    Add chain methods so that I can sync the async API calls

--- src/test/java/be/howest/ti/mars/logic/controller/BridgeTest.java
+++ src/test/java/be/howest/ti/mars/logic/controller/BridgeTest.java
@@ -14,6 +14,7 @@ import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -24,6 +25,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(VertxExtension.class)
 public class BridgeTest {
+
+    // i wrote my own chain method so that i could launch requests sync using these async methods
 
     // config
     private static final int PORT = 8080;
@@ -40,6 +43,7 @@ public class BridgeTest {
             .put("businessAccount", false)
             .put("homeAddress", " ")
             .put("homeEndpointId", 1);
+
     private static final JsonObject loginAccountJson = new JsonObject()
             .put("name", DEFAULT_PLAYER_NAME)
             .put("password", DEFAULT_PASS_WORD)
@@ -52,23 +56,22 @@ public class BridgeTest {
     private static final Predicate<String> IGNORE_BODY = body -> true;
     private static final JsonObject INVALID_BODY = new JsonObject().put("random", "data");
     private static String token;
+
     // utils
     private Vertx vertx;
     private WebClient webClient;
 
+    private static String trimBody(String body) {
+        return body.substring(1, body.length() - 1);
+    }
+
     @BeforeEach
     void deploy(final VertxTestContext testContext) {
         vertx = Vertx.vertx();
-        vertx.deployVerticle(
-                new WebServer(),
-                testContext.succeedingThenComplete());
+        vertx.deployVerticle(new WebServer());
         webClient = WebClient.create(vertx);
 
-        //add acc
-        testRequest(testContext, HttpMethod.POST, "createAccount", null, loginAccountJson,
-                200, IGNORE_BODY);
-        //login acc
-        login(testContext);
+        createAccount(testContext, loginAccountJson, () -> login(testContext, testContext::completeNow));
     }
 
     @AfterEach
@@ -88,7 +91,7 @@ public class BridgeTest {
         return request;
     }
 
-    private void testRequest(
+    private void chainEnd(
             final VertxTestContext testContext,
             HttpMethod method, String requestURI, String authorizationHeader, Object body,
             int expectedStatusCode, Predicate<String> isExpectedBody
@@ -103,10 +106,8 @@ public class BridgeTest {
         ));
     }
 
-    private void testRequest(
-            final VertxTestContext testContext,
-            HttpMethod method, String requestURI, String authorizationHeader,
-            int expectedStatusCode, Predicate<String> isExpectedBody
+    private void chainEnd(final VertxTestContext testContext, HttpMethod method, String requestURI, String authorizationHeader,
+                          int expectedStatusCode, Predicate<String> isExpectedBody
     ) {
         makeRequest(method, requestURI, authorizationHeader).send(testContext.succeeding(response ->
                 testContext.verify(() -> {
@@ -119,46 +120,132 @@ public class BridgeTest {
                     testContext.completeNow();
                 })
         ));
+
     }
 
+    private void chain(final VertxTestContext testContext, HttpMethod method, String requestURI, String authorizationHeader,
+                       int expectedStatusCode, Predicate<String> isExpectedBody, Runnable runnable
+    ) {
+        makeRequest(method, requestURI, authorizationHeader).send(testContext.succeeding(response ->
+                testContext.verify(() -> {
+                    assertEquals(expectedStatusCode, response.statusCode());
+                    String body = response.bodyAsString();
+                    assertTrue(
+                            isExpectedBody.test(body),
+                            () -> String.format("Unexpected body: %s", body)
+                    );
+                    runnable.run();
+                })
+        ));
+    }
+
+    private void chain(
+            final VertxTestContext testContext,
+            HttpMethod method, String requestURI, String authorizationHeader, Object body,
+            int expectedStatusCode, Predicate<String> isExpectedBody, Runnable runnable
+    ) {
+        // Do as expected:
+        makeRequest(method, requestURI, authorizationHeader).sendJson(body, testContext.succeeding(response ->
+                testContext.verify(() -> {
+                    assertEquals(expectedStatusCode, response.statusCode());
+                    assertTrue(isExpectedBody.test(response.bodyAsString()));
+                    runnable.run();
+                })
+        ));
+    }
+
+
     @Test
-    void startWebServer(final VertxTestContext testContext) {
+    public void startWebServer(final VertxTestContext testContext) {
         testContext.completeNow();
     }
 
     @Test
-    void getMessageReturnsAWelcomeMessage(final VertxTestContext testContext) {
-        testRequest(testContext, HttpMethod.GET, "message", null, 200, body -> body.equals("\"SmellyEllie\""));
+    @Disabled
+    //example of why it was needed, it goes before -> after -> middle -> actually after, chaining allows me to execute methods when the previous async completes instead of firing at same time
+    public void getMessageReturnsAWelcomeMessage(final VertxTestContext testContext) {
+        System.out.println("before");
+        chain(testContext, HttpMethod.GET, "message", null, 200, body -> trimBody(body).equals("SmellyEllie"), () -> {
+            System.out.println("middle");
+            chain(testContext, HttpMethod.GET, "message", null, 200, body -> trimBody(body).equals("SmellyEllie"), () -> {
+                System.out.println("actually after");
+                testContext.completeNow();
+            });
+        });
+        System.out.println("after");
+    }
+
+    private void createAccount(VertxTestContext testContext, JsonObject body, Runnable runnable) { // Reusability
+        chain(testContext, HttpMethod.POST, "createAccount", null, body, 200, IGNORE_BODY, runnable);
     }
 
     @Test
-    void createAccount(final VertxTestContext testContext) {
-        testRequest(testContext, HttpMethod.POST, "createAccount", null, createAccountJson,
-                200, IGNORE_BODY);
+    public void createAccount(final VertxTestContext testContext) {
+        createAccount(testContext, createAccountJson, testContext::completeNow);
     }
 
     @Test
-    void createAccountInvalidBody(final VertxTestContext testContext) {
-        testRequest(testContext, HttpMethod.POST, "createAccount", null, INVALID_BODY,
+    public void createAccountInvalidBody(final VertxTestContext testContext) {
+        chainEnd(testContext, HttpMethod.POST, "createAccount", null, INVALID_BODY,
                 400, IGNORE_BODY);
     }
 
-    @Test
-    void login(final VertxTestContext testContext) {
-        testRequest(testContext, HttpMethod.POST, "login", null, loginBodyJson, 200, body -> {
-            token = AUTHORIZATION_TOKEN_PREFIX + body;
+    private void login(final VertxTestContext testContext, Runnable runnable) {
+        chain(testContext, HttpMethod.POST, "login", null, loginBodyJson, 200, body -> {
+            token = AUTHORIZATION_TOKEN_PREFIX + trimBody(body);
+            System.out.println("set token: " + token);
             return true;
-        });
+        }, runnable);
     }
 
     @Test
-    void logoutInvalidToken(final VertxTestContext testContext) {
-        testRequest(testContext, HttpMethod.DELETE, "login", null, 401, IGNORE_BODY);
+    public void login(final VertxTestContext testContext) {
+        login(testContext, testContext::completeNow);
     }
 
     @Test
-    void logout(final VertxTestContext testContext) {
-        testRequest(testContext, HttpMethod.DELETE, "login", null, 401, IGNORE_BODY);
+    public void logoutInvalidToken(final VertxTestContext testContext) {
+        chainEnd(testContext, HttpMethod.DELETE, "login", null, 401, IGNORE_BODY);
     }
+
+    @Test
+    public void logout(final VertxTestContext testContext) {
+        chainEnd(testContext, HttpMethod.DELETE, "login", token, 200, IGNORE_BODY);
+    }
+
+    private void shareLocation(final VertxTestContext testContext, HttpMethod method, int code, Runnable chain) {
+        chain(testContext, HttpMethod.POST, "shareLocation", token, code, IGNORE_BODY, chain);
+    }
+
+    @Test
+    public void shareLocation(final VertxTestContext testContext) {
+        System.out.println("share loc: " + token);
+        shareLocation(testContext, HttpMethod.POST, 200, testContext::completeNow);
+    }
+
+    private void stopShareLocation(final VertxTestContext testContext, int code, Runnable chain) {
+        shareLocation(testContext, HttpMethod.DELETE, code, chain);
+    }
+
+    @Test
+    public void stopShareLocation(final VertxTestContext testContext) {
+        shareLocation(testContext, HttpMethod.POST, 200, () -> stopShareLocation(testContext, 200, testContext::completeNow));
+    }
+
+
+    @Test
+    @Disabled
+    // decided against validating these as they are harmless
+    public void stopShareLocationInvalid(final VertxTestContext testContext) {
+        stopShareLocation(testContext, 402, testContext::completeNow);
+    }
+
+    @Test
+    @Disabled
+    public void shareLocationInvalid(final VertxTestContext testContext) {
+        shareLocation(testContext, HttpMethod.POST, 200, () -> shareLocation(testContext, HttpMethod.POST, 402, testContext::completeNow));
+    }
+
+
 }
 
