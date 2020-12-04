1ee1f3fb4bf01d8629c426e075a4252964cb3d30
maarten.vercruysse <maarten.vercruysse@student.howest.be>
2020-12-04 04:37:52 +0100

    Add possible attempt 3 to fix pipeline issue

--- src/test/java/be/howest/ti/mars/logic/controller/accounts/UserAccountTest.java
+++ src/test/java/be/howest/ti/mars/logic/controller/accounts/UserAccountTest.java
@@ -5,6 +5,7 @@ import be.howest.ti.mars.logic.data.repositories.AccountsRepository;
 import be.howest.ti.mars.logic.data.repositories.FavoritesRepository;
 import be.howest.ti.mars.logic.data.repositories.FriendsRepository;
 import be.howest.ti.mars.logic.data.util.MarsConnection;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -35,6 +36,11 @@ class UserAccountTest {
         repo.addUser(testDebby);
         repo.addUser(testPol);
 
+    }
+
+    @AfterAll
+    static void stop(){
+        MarsConnection.getInstance().cleanUp();
     }
 
 
