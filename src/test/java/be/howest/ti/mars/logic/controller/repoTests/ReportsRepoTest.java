4f8feee5a5cf53c6ea6c2dce516b842ce4a0840b
stijn.van.der.herten <stijn.van.der.herten@student.howest.be>
2020-12-10 17:11:11 +0100

    Implemented ReportsRepoTest file + integrated test methods.

---
+++ src/test/java/be/howest/ti/mars/logic/controller/repoTests/ReportsRepoTest.java
@@ -0,0 +1,56 @@
+package be.howest.ti.mars.logic.controller.repoTests;
+
+import be.howest.ti.mars.logic.controller.MTTSController;
+import be.howest.ti.mars.logic.controller.accounts.BusinessAccount;
+import be.howest.ti.mars.logic.controller.accounts.UserAccount;
+import be.howest.ti.mars.logic.controller.exceptions.EntityNotFoundException;
+import be.howest.ti.mars.logic.data.Repositories;
+import be.howest.ti.mars.logic.data.repositories.AccountsRepository;
+import be.howest.ti.mars.logic.data.util.MarsConnection;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReportsRepoTest {
+    private static final AccountsRepository accountRepo = Repositories.getAccountsRepo();
+    private static final UserAccount testDanny = new UserAccount("Danny", "Danny", 5, "MarsStreet 69");
+    private static final UserAccount testDebby = new UserAccount("Debby", "Debby", 3, "WestStreet 420");
+    private static final BusinessAccount testPol = new BusinessAccount("Pol", "Pol", 6, "Earthstreet 23");
+
+    @BeforeAll
+    static void start() {
+
+        try {
+            MarsConnection.configure("jdbc:h2:~/mars-db", "", "", 9000);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        accountRepo.addUser(testDanny);
+        accountRepo.addUser(testDebby);
+        accountRepo.addBusiness(testPol);
+    }
+
+    @AfterAll
+    static void end() {
+        MarsConnection.getInstance().cleanUp();
+    }
+
+
+    @Test
+    void getReportSections() {
+        assertEquals(4, Repositories.getReportsRepo().getReportSections().size());
+        System.out.println(Repositories.getReportsRepo().getReportSections());
+    }
+
+    @Test
+    void addReport() {
+        assertThrows(EntityNotFoundException.class, ()-> Repositories.getReportsRepo().addReport(testDanny,"Nothing","Very Bad service"));
+        assertDoesNotThrow(()-> Repositories.getReportsRepo().addReport(testDanny, "Other","Nothing to report it is amazing."));
+
+    }
+}
