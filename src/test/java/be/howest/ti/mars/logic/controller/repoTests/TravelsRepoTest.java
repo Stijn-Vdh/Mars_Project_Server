178070dd06d02b0dd4ac987f3aec7e3727058e9c
stijn.van.der.herten <stijn.van.der.herten@student.howest.be>
2020-12-10 16:18:07 +0100

    Implemented test code for travel functionalities

--- src/test/java/be/howest/ti/mars/logic/controller/repoTests/TravelsRepoTest.java
+++ src/test/java/be/howest/ti/mars/logic/controller/repoTests/TravelsRepoTest.java
@@ -3,17 +3,18 @@ package be.howest.ti.mars.logic.controller.repoTests;
 import be.howest.ti.mars.logic.controller.MTTSController;
 import be.howest.ti.mars.logic.controller.accounts.BusinessAccount;
 import be.howest.ti.mars.logic.controller.accounts.UserAccount;
+import be.howest.ti.mars.logic.controller.exceptions.DatabaseException;
+import be.howest.ti.mars.logic.controller.exceptions.EndpointException;
 import be.howest.ti.mars.logic.data.Repositories;
 import be.howest.ti.mars.logic.data.repositories.AccountsRepository;
 import be.howest.ti.mars.logic.data.util.MarsConnection;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.sql.SQLException;
+import java.util.LinkedList;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class TravelsRepoTest {
 
     private static final AccountsRepository accountRepo = Repositories.getAccountsRepo();
@@ -43,16 +44,23 @@ class TravelsRepoTest {
 
 
     @Test
+    @Order(2)
     void getTravelHistory() {
-
+        assertEquals(1, Repositories.getTravelsRepo().getTravelHistory(testDanny).size());
     }
 
     @Test
+    @Order(1)
     void travel() {
-
+        assertThrows(EndpointException.class, ()-> controller.travel(testDanny,1,1,"standard"));
+        controller.travel(testDanny, 1,2,"standard");
     }
 
     @Test
+    @Order(3)
     void cancelTravel() {
+        assertThrows(DatabaseException.class, ()-> controller.cancelTrip(testDanny,2));
+        controller.cancelTrip(testDanny, 1);
+        assertEquals(0,Repositories.getTravelsRepo().getTravelHistory(testDanny).size());
     }
 }
